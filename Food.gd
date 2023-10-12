bde8bb419fd771e8c665c94e2112d715bd9e5d41
Erin Nova <erin@the-system.eu.org>
2023-10-12 11:23:12 -0400

    Curve food towards pig

--- Food.gd
+++ Food.gd
@@ -1,14 +1,34 @@
 extends KinematicBody2D
 
-export (int) var speed = 300
+export (float) var friction = 0.18
 
-var velocity = Vector2()
 var detect = true
+var direction = Vector2.ZERO
+var _velocity = Vector2.ZERO
+var t = 0.0
+onready var start = self.position
 
-func _on_SwipeDetector_swiped(direction):
+func _on_SwipeDetector_swiped(dir):
 	if detect:
-		velocity = -direction * speed
+		#dir.y = self.global_position.y - 500
+		direction = -dir
 		detect = false
 
+func generate_curve(t: float):
+	var target = Vector2(100, 500)
+	#var middle = start.linear_interpolate(target, 0.5)
+	var middle = Vector2.ZERO
+	middle.x = target.x+100
+	middle.y = target.y - (target.y - start.y)
+	return _quadratic_bezier(start, middle, target, t)
+
+func _quadratic_bezier(p0: Vector2, p1: Vector2, p2: Vector2, t: float):
+	var q0 = lerp(p0, p1, t)
+	var q1 = lerp(p1, p2, t)
+	var r = lerp(q0, q1, t)
+	return r
+
 func _physics_process(delta):
-	move_and_slide(velocity)
+	if !detect:
+		t += delta
+		position = generate_curve(t)
