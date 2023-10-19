0de65a98fa642698849a686c9220122a93d9e305
IroNinja5 <colinvandermeer@gmail.com>
2023-10-19 12:12:03 -0400

    Clean up swipe detector code

--- SwipeDetector.gd
+++ SwipeDetector.gd
@@ -6,22 +6,15 @@ signal swipe_canceled(start_position)
 export (float, 1.0, 1.5) var MAX_DIAGONAL_SLOPE = 1.3
 
 onready var timer = $Timer
-onready var food_shape = get_owner().get_node("Collision")
 var swipe_start_position = Vector2()
-var mouse_on = false
 
 func _on_Food_input_event(viewport, event, shape_idx):
-#	pass # Replace with function body.
-#func _input_event(viewport, event, food_shape):
 	if event is InputEventMouseButton:
-		print("clicked")
 		if event.is_pressed():
-			print("detected")
 			_start_detection(event.position)
 func _input(event):
 	if event is InputEventMouseButton:
 		if not timer.is_stopped():
-			print("stoped")
 			_end_detection(event.position)
 		
 func _start_detection(position):
@@ -41,13 +34,3 @@ func _end_detection(position):
 
 func _on_Timer_timeout():
 	emit_signal('swipe_canceled', swipe_start_position)
-
-
-func _on_Food_mouse_entered():
-	mouse_on = true
-
-func _on_Food_mouse_exited():
-	mouse_on = false
-
-
-
