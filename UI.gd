0e06c930981fd7d727e5e5108f82a2ef3f390753
IroNinja5 <colinvandermeer@gmail.com>
2023-10-20 10:15:38 -0400

    Add countdown timer and wip death screen

--- UI.gd
+++ UI.gd
@@ -1,5 +1,18 @@
 extends Control
 
+export(PackedScene) var menu
 
+func _ready():
+	$ProgressBar.value = 7  # Start with full progress
+	$ProgressBar.max_value = 7
+
+func _on_Timer_timeout():
+	$ProgressBar.value -= 1   # Update the progress bar
+	if $ProgressBar.value <= 0:
+		get_tree().change_scene_to(menu)
+		# You can add your logic for what happens when the countdown reaches 0 here
+
+	
 func _on_Pig_update_consumed(c):
-	$Panel/Label.text = "Consumed: " + str(c)
+	$ProgressBar.value += 1.5
+#	$Panel/Label.text = "Consumed: " + str(c)
