e5005c01838e261abb0c6eba9bd9a1ea9405bbf2
IroNinja5 <colinvandermeer@gmail.com>
2023-10-31 09:42:14 -0400

    Make food more modular

--- AnimatedSprite.gd
+++ AnimatedSprite.gd
@@ -2,16 +2,16 @@ extends AnimatedSprite
 
 var type = true
 
+# NEED BAD FOOD AFTER CAROT AND ABOVE
+var foods = ["apple", "bottle", "broccoli", "meat", "carrot", "cheese", "granola", "grapes", "pasta", "sandwich"]
+
 func _ready():
 	randomize()
 	play_random_animation()
 
 func play_random_animation():
-	var animations = frames.get_animation_names()
-	var animation_id = randi() % animations.size()
-	if ScoreManager.score < 30 and animation_id > 1:
-		animation_id = animation_id / 2
-	var animation_name = animations[animation_id]
+	var animation_id = randi() % (2 + int(ScoreManager.score / 10))
+	var animation_name = foods[animation_id]
 	if animation_name == "bottle":
 		type = false
 	if animation_name == "meat":
