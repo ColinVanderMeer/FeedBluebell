c61615a1cf3de3c23d8464cc0a37d0ffc6d0acf2
IroNinja5 <colinvandermeer@gmail.com>
2023-10-31 09:47:56 -0400

    Add new sprites

--- AnimatedSprite.gd
+++ AnimatedSprite.gd
@@ -3,17 +3,18 @@ extends AnimatedSprite
 var type = true
 
 # NEED BAD FOOD AFTER CAROT AND ABOVE
-var foods = ["apple", "bottle", "broccoli", "meat", "carrot", "cheese", "granola", "grapes", "pasta", "sandwich"]
+var foods = ["apple", "bottle", "broccoli", "meat", "carrot", "wrapper", "cheese", "spoon", "grapes", "granola", "pasta", "sandwich"]
 
 func _ready():
 	randomize()
 	play_random_animation()
 
 func play_random_animation():
-	var animation_id = randi() % (2 + int(ScoreManager.score / 10))
+	var animation_id = randi() % (2 + int(ScoreManager.score / 5))
 	var animation_name = foods[animation_id]
-	if animation_name == "bottle":
-		type = false
-	if animation_name == "meat":
+	if animation_name == "bottle"\
+	or animation_name == "meat"\
+	or animation_name == "wrapper"\
+	or animation_name == "spoon":
 		type = false
 	play(animation_name)
