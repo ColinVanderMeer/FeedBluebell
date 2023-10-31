ded0f119905e0b7287a1879f2310b65c5cf042c3
Erin Nova <erin@the-system.eu.org>
2023-10-31 10:08:45 -0400

    Consume food SFX

--- Trash.gd
+++ Trash.gd
@@ -3,6 +3,17 @@ extends StaticBody2D
 var consumed = 0
 signal update_consumed(c)
 
+export(AudioStream) var good_food
+export(AudioStream) var bad_food
+
 func _on_Area2D_body_entered(body):
+	if !body.type:
+		$FoodSFX.stream = good_food
+		$FoodSFX.volume_db = -5.0
+	else:
+		$FoodSFX.stream = bad_food
+		$FoodSFX.volume_db = 5.0
+	$FoodSFX.play()
 	emit_signal("update_consumed", body.type)
 	body.queue_free()
+
