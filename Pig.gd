4bfa450970e3513a0d41cbeb7bc812fe4b8a3d06
IroNinja5 <colinvandermeer@gmail.com>
2023-11-02 14:45:29 -0400

    The pig now feels emotion

--- Pig.gd
+++ Pig.gd
@@ -7,9 +7,17 @@ func _on_Area2D_body_entered(body):
 	if body.type:
 		$FoodSFX.stream = SoundManager.good_food
 		$FoodSFX.volume_db = -5.0
+		$AnimatedSprite.play("happy")
+		$Timer.start()
 	else:
 		$FoodSFX.stream = SoundManager.bad_food
 		$FoodSFX.volume_db = 5.0
+		$AnimatedSprite.play("sad")
+		$Timer.start()
 	$FoodSFX.play()
 	emit_signal("update_consumed", body.type)
 	body.queue_free()
+
+
+func _on_Timer_timeout():
+	$AnimatedSprite.play("neutral")
