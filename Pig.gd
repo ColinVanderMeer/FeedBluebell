f98a42f44e9c7deb403c95b79440fa90607955ff
Erin Nova <erin@the-system.eu.org>
2023-12-06 14:36:50 -0500

    Have circle ping on feeding

--- Pig.gd
+++ Pig.gd
@@ -4,9 +4,21 @@ extends StaticBody2D
 var consumed = 0
 signal update_consumed(type)
 
+var ping = false
+var scaling = Vector2(0.025,0.025)
+var max_scale = Vector2(0.5,0.5)
+
 func _process(delta):
 	# Ensure always on the bottom of screen
 	self.position.y = get_viewport().get_visible_rect().size.y - 150
+	if ping and ($Ping.scale < max_scale):
+		$Ping.scale += scaling
+		$Ping.modulate.a -= 0.05
+	elif $Ping.scale >= max_scale:
+		$Ping.scale = scaling
+		$Ping.modulate.a = 1
+		ping = false
+
 
 func _on_Area2D_body_entered(body):
 	if body.type:
@@ -14,11 +26,17 @@ func _on_Area2D_body_entered(body):
 		$FoodSFX.volume_db = -5.0 # TODO: why?
 		$AnimatedSprite.play("happy") # TODO: use better animation system
 		$Timer.start()
+		
+		$Ping.modulate = Color("#84f174")
+		ping = true
 	else:
 		$FoodSFX.stream = SoundManager.bad_food
 		$FoodSFX.volume_db = 5.0
 		$AnimatedSprite.play("sad")
 		$Timer.start()
+		
+		$Ping.modulate = Color("#f17486")
+		ping = true
 
 	$FoodSFX.play()
 	emit_signal("update_consumed", body.type)
@@ -26,4 +44,3 @@ func _on_Area2D_body_entered(body):
 
 func _on_Timer_timeout():
 	$AnimatedSprite.play("neutral")
-
--- Trash.gd
+++ Trash.gd
@@ -3,16 +3,34 @@ extends StaticBody2D
 var consumed = 0
 signal update_consumed(c)
 
+var ping = false
+var scaling = Vector2(0.025,0.025)
+var max_scale = Vector2(0.5,0.5)
+
 func _on_Area2D_body_entered(body):
 	if !body.type:
 		$FoodSFX.stream = SoundManager.good_food
 		# TODO: remove these, and make sure audio is normalized
 		$FoodSFX.volume_db = -5.0
+		
+		$Ping.modulate = Color("#84f174")
+		ping = true
 	else:
 		$FoodSFX.stream = SoundManager.bad_food
 		$FoodSFX.volume_db = 5.0
+		
+		$Ping.modulate = Color("#f17486")
+		ping = true
 	
 	$FoodSFX.play()
 	emit_signal("update_consumed", body.type)
 	body.queue_free()
 
+func _process(delta):
+	if ping and ($Ping.scale < max_scale):
+		$Ping.scale += scaling
+		$Ping.modulate.a -= 0.05
+	elif $Ping.scale >= max_scale:
+		$Ping.scale = scaling
+		$Ping.modulate.a = 1
+		ping = false
