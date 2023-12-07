a1ab4ddc783339c66a759f16ae124afc09d1de5e
IroNinja5 <colinvandermeer@gmail.com>
2023-12-07 14:53:23 -0500

    Added working volume sliders

--- Settings.gd
+++ Settings.gd
@@ -5,11 +5,27 @@ extends Control
 # var a = 2
 # var b = "text"
 
-
 # Called when the node enters the scene tree for the first time.
 func _on_BackButton_pressed():
 	self.visible = false
 
+
+onready var musicBus := AudioServer.get_bus_index("Music")
+onready var sfxBus := AudioServer.get_bus_index("SFX")
+
+
+
+
+func _ready() -> void:
+	$Panel/MusicSlider.value = db2linear(AudioServer.get_bus_volume_db(musicBus))
+	$Panel/SfxSlider.value = db2linear(AudioServer.get_bus_volume_db(sfxBus))
+
+
+func _on_MusicSlider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(musicBus, linear2db(value))
+
+func _on_SfxSlider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(sfxBus, linear2db(value))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
