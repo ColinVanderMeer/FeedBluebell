a240e5033ab910c4d893de78b8ffc65205dffda2
IroNinja5 <colinvandermeer@gmail.com>
2023-12-11 16:13:20 -0500

    Adjust game difficulty (hard)

--- Game.gd
+++ Game.gd
@@ -12,7 +12,7 @@ func _on_SpawnTimer_timeout():
 	new_food.position = $SpawnPosition.global_position
 	new_food.pig_target = $Pig.global_position
 	new_food.trash_target = $Trash.global_position
-	new_food.FALL_SPEED = 5 + ScoreManager.score / 20 # TODO: make this better
+	new_food.FALL_SPEED = 5 + ScoreManager.score / 17 # TODO: make this better
 	add_child(new_food)
 
 func _process(_delta):
@@ -20,7 +20,7 @@ func _process(_delta):
 		$SpawnTimer.paused = true
 	else:
 		$SpawnTimer.paused = false
-		$SpawnTimer.wait_time = 1 - log(ScoreManager.score) / 30
+		$SpawnTimer.wait_time = 1 - log(ScoreManager.score) / 20
 	$Background2.position.y = get_viewport().get_visible_rect().size.y - BGSIZE
 
 func _on_Despawn_body_entered(body):
