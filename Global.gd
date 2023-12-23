e51581707ace78bf270b734e22c7795656f16fd3
ColinVanderMeer <colinvandermeer@gmail.com>
2023-12-22 19:21:52 -0500

    Scale all UI assets by 0.5 and general asset cleanup

--- Global.gd
+++ Global.gd
@@ -7,7 +7,7 @@ var skin_data = ["Default"]
 var unlock_data = [music_data, sound_data, skin_data]
 
 # Modify these to add additional soundpack settings
-enum SOUND_PACK {DEFAULT, JADEN}
+enum SOUND_PACK {DEFAULT}
 
 # Set the default soundpack
 var soundpack = SOUND_PACK.DEFAULT
@@ -21,37 +21,37 @@ var bad_food
 var game_over
 
 func _ready():
-    load_data()
+	load_data()
 
-    # Dynamically set the file of each SFX depending on soundpack folder
-    var path = "res://assets/sfx/"
-    match soundpack:
-        SOUND_PACK.DEFAULT:
-            path += "default/"
-        SOUND_PACK.JADEN:
-            path += "jaden/"
+	# Dynamically set the file of each SFX depending on soundpack folder
+	var path = "res://assets/sfx/"
+	match soundpack:
+		SOUND_PACK.DEFAULT:
+			path += "default/"
+		SOUND_PACK.JADEN:
+			path += "jaden/"
 
-    # TODO: Find better way to do this, rather than going through each manually
-    good_food = load(path+"good_food.wav")
-    bad_food = load(path+"bad_food.wav")
-    game_over = load(path+"game_over.wav")
+	# TODO: Find better way to do this, rather than going through each manually
+	good_food = load(path+"good_food.wav")
+	bad_food = load(path+"bad_food.wav")
+	game_over = load(path+"game_over.wav")
 
 var score = 0
 var pause = false # this shouldn't be here, too bad
 
 func save_data():
-    var file = File.new()
-    file.open(SAVE_FILE, File.WRITE)
-    file.store_var(unlock_data)
-    file.close()
+	var file = File.new()
+	file.open(SAVE_FILE, File.WRITE)
+	file.store_var(unlock_data)
+	file.close()
 
 func load_data():
-    var file = File.new()
-    if not file.file_exists(SAVE_FILE):
-        music_data = ["My Soul Cries Out"]
-        sound_data = ["Default"]
-        skin_data = ["Default"]
-        save_data()
-    file.open(SAVE_FILE, File.READ)
-    unlock_data = file.get_var()
-    file.close()
+	var file = File.new()
+	if not file.file_exists(SAVE_FILE):
+		music_data = ["My Soul Cries Out"]
+		sound_data = ["Default"]
+		skin_data = ["Default"]
+		save_data()
+	file.open(SAVE_FILE, File.READ)
+	unlock_data = file.get_var()
+	file.close()
