747ba1a60d665661e626691f4621aeb1cf5e8fdb
ColinVanderMeer <colinvandermeer@gmail.com>
2023-12-25 11:31:27 -0500

    Make saving ACTUALLY work

--- Global.gd
+++ Global.gd
@@ -21,33 +21,38 @@ var bad_food
 var game_over
 
 func _ready():
-    load_data()
+	load_data()
 
-    # Dynamically set the file of each SFX depending on soundpack folder
-    var path = "res://assets/sfx/"
-    match soundpack:
-        SOUND_PACK.DEFAULT:
-            path += "default/"
+	# Dynamically set the file of each SFX depending on soundpack folder
+	var path = "res://assets/sfx/"
+	match soundpack:
+		SOUND_PACK.DEFAULT:
+			path += "default/"
 
-    # TODO: Find better way to do this, rather than going through each manually
-    good_food = load(path+"good_food.wav")
-    bad_food = load(path+"bad_food.wav")
-    game_over = load(path+"game_over.wav")
+	# TODO: Find better way to do this, rather than going through each manually
+	good_food = load(path+"good_food.wav")
+	bad_food = load(path+"bad_food.wav")
+	game_over = load(path+"game_over.wav")
 
 var score = 0
 var pause = false # this shouldn't be here, too bad
 
 func save_data():
-    unlock_data = [music_data, sound_data, skin_data]
-    var file = File.new()
-    file.open(SAVE_FILE, File.WRITE)
-    file.store_var(unlock_data)
-    file.close()
+	unlock_data = [music_data, sound_data, skin_data]
+	var file = File.new()
+	file.open(SAVE_FILE, File.WRITE)
+	file.store_var(unlock_data)
+	file.close()
 
 func load_data():
-    var file = File.new()
-    if not file.file_exists(SAVE_FILE):
-        save_data()
-    file.open(SAVE_FILE, File.READ)
-    unlock_data = file.get_var()
-    file.close()
+	var file = File.new()
+	if not file.file_exists(SAVE_FILE):
+		save_data()
+	file.open(SAVE_FILE, File.READ)
+	unlock_data = file.get_var()
+	file.close()
+	
+	music_data = unlock_data[0]
+	sound_data = unlock_data[1]
+	skin_data = unlock_data[2]
+	
