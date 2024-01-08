5ad9644bc200966122919ddf1486a76eebba6ce8
IroNinja5 <colinvandermeer@gmail.com>
2024-01-08 10:49:17 -0500

    Implement new sprites

--- AnimatedSprite.gd
+++ AnimatedSprite.gd
@@ -5,15 +5,15 @@ var animation_id = 0
 
 # NEED BAD FOOD AFTER CAROT AND ABOVE
 # TODO: what the hell is this
-var foods = ["apple", "bottle", "cookie", "milk", "rockMeat", "carrot", "wrapper", "cheese", "sandwichBag", "grapes", "meat", "granola", "fish", "pasta", "cupcake", "sandwich", "spoon", "cookieBag", "broccoli"]
+var foods = ["apple", "bottle", "cookie", "milk", "rockMeat", "carrot", "wrapper", "cheese", "sandwichBag", "grapes", "meat", "granola", "fish", "pasta", "cupcake", "sandwich", "spoon", "cookieBag", "broccoli", "tim", "cupcakeUnwrapped", "rockFish"]
 
 func _ready():
 	randomize()
 	play_random_animation()
 
 func play_random_animation():
-	if int(Global.score / 5) > 17:
-		animation_id = randi() % 19
+	if int(Global.score / 5) > 20:
+		animation_id = randi() % 22
 	else:	
 		animation_id = randi() % (2 + int(Global.score / 5))
 	var animation_name = foods[animation_id]
@@ -26,6 +26,7 @@ func play_random_animation():
 	or animation_name == "fish"\
 	or animation_name == "sandwichBag"\
 	or animation_name == "cookieBag"\
-	or animation_name == 'cupcake':
+	or animation_name == "tim"\
+	or animation_name == "cupcake":
 		type = false
 	play(animation_name)
